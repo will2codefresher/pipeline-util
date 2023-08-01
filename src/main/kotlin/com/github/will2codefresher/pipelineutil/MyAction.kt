7f980083b919edfde20ea5843ff0556eeba8635e
GitHub Action <action@github.com>
2023-08-01 11:25:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/will2codefresher/pipelineutil/MyAction.kt
+++ src/main/kotlin/com/github/will2codefresher/pipelineutil/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.will2codefresher.pipelineutil
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
--- src/main/kotlin/com/github/will2codefresher/pipelineutil/MyBundle.kt
+++ src/main/kotlin/com/github/will2codefresher/pipelineutil/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.will2codefresher.pipelineutil
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
